1d3962ef394b356253e76602376fff3da58e4858
carozzajoao <129419461+carozzajoao@users.noreply.github.com>
2023-03-30 14:37:30 -0300

    Estudo de Caso

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+Estudo de Caso (João Pedro Carozza e Guilherme Bicciato Rossetti) imagem
 
+Empresa Oher
+A Other, é uma plataforma de streaming nova no mercado, fechamos parcerias com grandes empresas como a Disney, a Amazon e a DC, podendo distribuir seus filmes e séries para os assinantes da nossa plataforma.
+Status do Projeto: Em Andamento
+Para planejar, usaremos a ferramenta “MuleSoft”, falamos sobre usar essa plataforma porque ela nos oferece agilidade para se conectar com aplicativos, dados e dispositivos e muito mais.
+Para codificar, usaremos a ferramenta “git”, que será útil para o controle de versão, incluindo os projetos comerciais e códigos fonte abertos.
+Para construir, usaremos a ferramenta “Maven”, que permite a criação, geração de dependências e distribuição de uma aplicação editando apenas um arquivo e utilizando conceitos de proteção sobre configuração.
+Para testar, usaremos a ferramenta “JUnit”, facilitando a criação e manutenção do código para a automação de testes com apresentação dos resultados.
+Para o lançamento, usaremos a ferramenta "Jenkins", que permitirá implementar softwares, tornando, assim, o processo de desenvolvimento mais ágil e eficiente.
+Para o deploy, usaremos a ferramenta “docker”, onde haverá uma forma de virtualizar aplicações no conceito de “containers” (conjunto de processos que são executados a partir de uma imagem, imagem esta que fornece todos os arquivos necessários), trazido da web ou de seu repositório interno uma imagem completa, incluindo todas as dependências necessárias para executar sua aplicação.
+Para operar, usaremos a ferramenta "ANSIBLE", é uma ferramenta que pode configurar sistemas, implantar aplicativos e provisionar recursos de nuvem.
+Para monitorar, usaremos a ferramenta “splunk>”, que possibilita coletar, indexar e analisar a informação gerada pelo negócio.
